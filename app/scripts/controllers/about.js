733c6d3c50fd0a8b0858c5541401671b587831d3
Richiearnold <richard@ambercouch.co.uk>
2014-06-28 12:45:51 +0100

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amberambercouchcoukApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the amberambercouchcoukApp
+ */
+angular.module('amberambercouchcoukApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amberambercouchcoukApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the amberambercouchcoukApp
+ */
+angular.module('amberambercouchcoukApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
